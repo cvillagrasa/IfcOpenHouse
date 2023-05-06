7c4b85118a9ec285642d8cde7850207f83eaaf89
Carlos Villagrasa <carvillasil@gmail.com>
2023-05-06 23:51:27 +0200

    Improving styles for panel info

--- viz/quarto_viz.js
+++ viz/quarto_viz.js
@@ -3,7 +3,7 @@ import { loadIfcCommon } from "./viz_common.mjs";
 
 async function loadIfc() {
   const ifcUrl = "https://cdn.jsdelivr.net/gh/cvillagrasa/IfcOpenHouse@latest/ifc/IfcOpenHouse.ifc";
-  loadIfcCommon(ifcUrl, "25vh");
+  loadIfcCommon(ifcUrl, "35vh");
 }
 
 loadIfc();
