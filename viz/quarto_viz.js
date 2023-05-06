a1cede8708f175c4844969f7c5e8063c98fcbe5e
Carlos Villagrasa <carvillasil@gmail.com>
2023-05-06 13:06:45 +0200

    IFC.js: transparent styles + info panel

--- viz/quarto_viz.js
+++ viz/quarto_viz.js
@@ -1,31 +1,9 @@
-import { Color } from "three";
-import { IfcViewerAPI } from "web-ifc-viewer";
+import { loadIfcCommon } from "./viz_common.mjs";
 
 
 async function loadIfc() {
-  const container = document.getElementById("ifcjs-container");
-  container.style.height = "25vh";
-
   const ifcUrl = "https://cdn.jsdelivr.net/gh/cvillagrasa/IfcOpenHouse@latest/ifc/IfcOpenHouse.ifc";
-  const viewer = new IfcViewerAPI({
-      container,
-      backgroundColor: new Color(0xa3adc9),
-  });
-  viewer.axes.setAxes();
-  const model = await viewer.IFC.loadIfcUrl(ifcUrl);
-  await viewer.shadowDropper.renderShadow(model.modelID);
-  window.onmousemove = () => viewer.IFC.selector.prePickIfcItem();
-  window.ondblclick = async () => {
-    const {modelID, stepId} = await viewer.IFC.selector.pickIfcItem(true);
-    const ifcType = model.getIfcType(stepId);
-    console.log(`#${stepId} ${ifcType}`);
-  }
-
-  viewer.clipper.active = true;
-
-  // Useful to play in the browser / debugging
-  window.viewer = viewer;
-  window.model = model;
+  loadIfcCommon(ifcUrl, "25vh");
 }
 
 loadIfc();
