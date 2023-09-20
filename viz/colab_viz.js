de9f2b725ed4b804e9c6ecab87d2b88a2ea0f2fd
Carlos Villagrasa <carvillasil@gmail.com>
2023-09-20 22:41:40 +0200

    Fix typo on loadIfcFromColab

--- viz/colab_viz.js
+++ viz/colab_viz.js
@@ -4,7 +4,7 @@ import { loadIfcCommon } from "./viz_common.mjs";
 async function loadIfcFromColab(width="900px", height="500px") {
   google.colab.kernel.comms.registerTarget('ifcjsviz', (comm, message) => {
     let ifcStr = message["data"]["ifcStr"];
-    loadIfc(ifcStr, true);
+    loadIfcCommon(ifcStr, height, width, true);
     const container = document.getElementById("ifcjs-container");
     container.style.width = width;
     container.style.height = height;
