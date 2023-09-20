9f763a3534d72c1ac96472e80033915b032c78f0
Carlos Villagrasa <carvillasil@gmail.com>
2023-09-20 22:13:39 +0200

    Dedicated script for Google Colab

---
+++ viz/colab_viz.js
@@ -0,0 +1,12 @@
+import { loadIfcCommon } from "./viz_common.mjs";
+
+
+async function loadIfcFromColab(width="900px", height="500px") {
+  google.colab.kernel.comms.registerTarget('ifcjsviz', (comm, message) => {
+    let ifcStr = message["data"]["ifcStr"];
+    loadIfc(ifcStr, true);
+    const container = document.getElementById("ifcjs-container");
+    container.style.width = width;
+    container.style.height = height;
+  });
+}
